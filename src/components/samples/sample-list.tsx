e22c5fbc44f7ae2354e283d20ea310bba131987c
Mohit Nayak <mohitn21@gmail.com>
2024-04-04 17:15:29 +0530

    lazy load skeleton for sample list

--- src/components/samples/sample-list.tsx
+++ src/components/samples/sample-list.tsx
@@ -9,7 +9,7 @@ const SampleList = async () => {
       {samplesData.map((sample) => (
         <Link
           href={`/preview/${sample.slug}`}
-          className="group relative z-10 block cursor-pointer overflow-hidden rounded-md border-4"
+          className="group relative z-10 block h-80 w-full cursor-pointer overflow-hidden rounded-md border-4"
           key={sample.id}
         >
           <Image
@@ -17,8 +17,12 @@ const SampleList = async () => {
             width={500}
             height={600}
             alt={sample.name}
+            className="relative z-10 h-auto w-full"
           />
-          <div className="ease-primary absolute bottom-0 w-full translate-y-full bg-gray-600/90 p-2 text-left text-primary-light opacity-0 transition-all duration-300 group-hover:-translate-y-0 group-hover:opacity-100">
+          <div className="absolute inset-0 flex animate-pulse items-center justify-center bg-gray-200">
+            <div className="text-gray-600">Loading preview...</div>
+          </div>
+          <div className="absolute bottom-0 w-full translate-y-full bg-gray-600/90 p-2 text-left text-primary-light opacity-0 transition-all duration-300 ease-primary group-hover:-translate-y-0 group-hover:opacity-100">
             <h6 className="font-medium">{sample.name}</h6>
             <p className="text-sm">{sample.description}</p>
           </div>
